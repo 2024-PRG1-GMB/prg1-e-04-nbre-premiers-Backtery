fa00a35856dec894a6f4a27fb4702c3625ed9acb
Backtery <mirkomadau1@gmail.com>
2024-10-07 17:55:56 +0200

    calcul nombre premier et affichage

--- main.cpp
+++ main.cpp
@@ -9,13 +9,40 @@ int main() {
 
     //saisie de l'utilisateur
     do {
-        cout << "entrer une valeur [2-1000] : " << endl;
+        cout << "entrer une valeur [2-1000] : ";
         cin >> limite;
 
     } while (limite < 2 || limite > 1000);
 
+    int count = 0;
+    // calcul des nombres premiers
+    for (int i = 2; i <= limite; i++) {
+            if (count == 5) {
+                cout << endl;
+                count = 0;
+            }
+            if (i % 2 != 0) {
+                if (i % 3 != 0) {
+                    cout << i << "    ";
+                    count++;
+                }
+            }
+
+    }
 
 
+    /*affichage en tableau
+    cout << "Voici la liste des nombres premiers" << endl;
+
+    for (int j = 0; j < limite; j++) {
+        for (int k = 0; k < 5; k++) {
+            cout << "*";
+            cout << "   ";
+        }
+        cout << endl;
+
+
+    };*/
 
 
 
